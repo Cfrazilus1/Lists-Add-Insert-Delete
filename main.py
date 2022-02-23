8b5ba70a6c95ff06ecaa34e42decfbb24b864432
Cfrazilus1 <99999698+Cfrazilus1@users.noreply.github.com>
2022-02-23 18:31:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+fave_movies = ['The Lion King', 'Encanto', 'Aladdin']
+
+print (len (fave_movies))
+
+fave_movies.append ("Jurassic Park")
+
+print (len (fave_movies))
+
+print (fave_movies)
+
+fave_movies.insert (1, "The Lion King 2")
+
+print (fave_movies)
+
+del (fave_movies [2])
+
+print (fave_movies)
+
+del (fave_movies [0])
+del (fave_movies [0])
+del (fave_movies [0])
+print (fave_movies)
